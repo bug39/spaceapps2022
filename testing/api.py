320d70c3c58cf0bfb79532b9d9451bbddf76014b
sebewe <fireon06@gmail.com>
2022-09-21 20:44:38 -0700

    updated testing

--- testing/api.py
+++ testing/api.py
@@ -5,6 +5,8 @@ ROOT_ENDPOINT = "https://ntrs.nasa.gov/api"
 get_endpoint = lambda path: ROOT_ENDPOINT + '/' + path
 search_point = get_endpoint("citations/search")
 query = {
-    'center': "JPL"
+    'abstract': 'pluto'
 }
-#response = requests.post(search_point, params=query)
+response = requests.post(search_point, params=query)
+with open('out.txt', 'w') as f:
+    json.dump(json.loads(response.content), f, indent=4, separators=(", ", ": "))
