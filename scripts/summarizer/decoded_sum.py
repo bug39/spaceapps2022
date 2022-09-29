916f3ca70a4bde4dc1621e5b10547f3f46598ac3
sebewe <fireon06@gmail.com>
2022-09-28 20:19:58 -0700

    updated scripts for easier test-use

--- scripts/summarizer/decoded_sum.py
+++ scripts/summarizer/decoded_sum.py
@@ -3,7 +3,7 @@ from transformers import PegasusTokenizer
 from pdfminer.high_level import extract_text
 
 def get_text(filepath):
-    text = extract_text('textrank.pdf')
+    text = extract_text(filepath)
     countlines = lambda txt: txt.count('\n')
     print(f"linecount: {countlines(text)}, charcount: {len(text)}")
     return text
@@ -15,13 +15,16 @@ def get_summary(text: str):
     pegasus_model = PegasusForConditionalGeneration.from_pretrained(model_name)
 
     tokens = pegasus_tokenizer(text, truncation=True, padding="longest",
-    return_tensors="pt", max_length=400)
+    return_tensors="pt", max_length=450, min_length=300)
 
     encoded_summary = pegasus_model.generate(**tokens)
 
     decoded_summary = pegasus_tokenizer.decode(
         encoded_summary[0],
         skip_special_tokens=True,
-        max_length=450)
+        max_length=500,
+        min_length=75)
 
     return decoded_summary
+
+#print(get_summary(get_text('tester2.pdf')))
