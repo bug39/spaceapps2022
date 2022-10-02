0e0480a3e30b642fa170dd389eb5eb5b0f1a3075
bug39 <105895978+bug39@users.noreply.github.com>
2022-10-02 12:06:02 -0700

    Update decoded_sum.py

--- scripts/decoded_sum.py
+++ scripts/decoded_sum.py
@@ -1,7 +1,9 @@
 from transformers import PegasusForConditionalGeneration
 from transformers import PegasusTokenizer
 from pdfminer.high_level import extract_text
-
+from keybert import KeyBERT
+spacy.cli.download("en_core_web_lg")
+import re
 def get_text(filepath):
     text = extract_text(filepath)
     countlines = lambda txt: txt.count('\n')
@@ -21,10 +23,21 @@ def get_summary(text: str):
 
     decoded_summary = pegasus_tokenizer.decode(
         encoded_summary[0],
-        skip_special_tokens=True,
-        max_length=500,
-        min_length=75)
+        skip_special_tokens=True)
 
     return decoded_summary
 
-#print(get_summary(get_text('tester2.pdf')))
+def get_keywords(text: str):
+    nlp = spacy.load("en_core_web_lg")
+    nlp.add_pipe("textrank")
+    doc = nlp(test)
+    
+    extract_text = ""
+    for i in doc._.textrank.summary(limit_sentences=5):
+        extract_text += str(i)
+
+    kw_model = KeyBERT()
+    return keywords = kw_model.extract_keywords(extract_text)
+    
+def cleanup(text: str):
+    return new_text = re.sub(r"[^a-zA-Z0-9 ]", "", decoded_summary)
